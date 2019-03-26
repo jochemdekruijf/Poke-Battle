184fc5488ab3c34cd02bc836abda6d86c2a1b414
Jochem de Kruijf <jochemdekruijfff@outlook.com>
2019-03-26 13:13:09 +0100

    background added

--- index.php
+++ index.php
@@ -5,9 +5,7 @@ require "pokemon.php";
 
 $pikachu =  new Pokemon('pickachu', '100', 'electric', 'water',"Pickachuuuuuuuuuuuuu!","40");
 
-print_r('<pre>'. $pikachu . '</pre>');
 
-$pikachu->sayOneliner();
 
 
 ?>
@@ -22,9 +20,9 @@ $pikachu->sayOneliner();
        <div id="pokemon">
            <img src="img/pikachu.gif" id="pikachu" alt="pikachu">
            <img src="img/charmeleon-b.gif" id="charmelon" alt="charmeleon">
-            <img src="img/background.png" id="background" alt="background">
+            <img src="img/Battle.png" id="battle" alt="background">
             <img src="img/gameboy.png" id="gameboy" alt="gameboy">
-           <p id="description_text">Sample <br>
+           <p id="description_text">Sample text<br>
                Attack was ineffetive,<br>
                damage reduced by 5.
 
